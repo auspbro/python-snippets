e3c0cc012fd31a09fd98003f2ef7ad288caacd67
Ryan Xue <auspbro@gmail.com>
2018-10-10 13:11:09 +0800

    Fix type error

--- ip_query.py
+++ ip_query.py
@@ -2,10 +2,14 @@
 # coding: utf-8
 
 import urllib2
+import json
 import ssl
 ssl._create_default_https_context = ssl._create_unverified_context
 
 
-url = "https://sp0.baidu.com/8aQDcjqpAAV3otqbppnN2DJv/api.php?query=223.104.210.104&co=&resource_id=6006"
+input_data = raw_input("请输入IP地址:")
+url = "https://sp0.baidu.com/8aQDcjqpAAV3otqbppnN2DJv/api.php?query=" + input_data + "&co=&resource_id=6006"
 response_data = urllib2.urlopen(url).read().decode('gbk')
-print(response_data)
+# print(response_data)
+
+print (json.loads(response_data)["data"][0]["location"])
